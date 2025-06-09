7cd5a138a6ab80d15622dd13775b452322032818
Mohsen Maleki Fard <mohsenmaleki1389@gmail.com>
2025-06-09 15:40:34 +0000

    Ubdate

--- TestApp/obj/Debug/net8.0/TestApp.AssemblyInfo.cs
+++ TestApp/obj/Debug/net8.0/TestApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a48ae3b3c028697b751952a1ea952a023cb9610")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+530348b2efae98408e43217f9b8b1ce45190a7aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
