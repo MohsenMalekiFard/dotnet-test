ce336389db001ba83052ebb302dc403484d74429
Mohsen Maleki Fard <mohsenmaleki1389@gmail.com>
2025-06-09 15:44:29 +0000

    Update Program File

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-int a = 10
+int a = 10;
 Console.WriteLine(a);
 
--- TestApp/obj/Debug/net8.0/TestApp.AssemblyInfo.cs
+++ TestApp/obj/Debug/net8.0/TestApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+530348b2efae98408e43217f9b8b1ce45190a7aa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23d25b7c7200274db37c14f0f97ec40b0263806e")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
